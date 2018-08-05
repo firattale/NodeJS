3064015cb57d4d54ddaf9812e259a600c687af1d
Firat Tale <talefirat@gmail.com>
2018-08-05 19:16:00 +0300

    change es6

--- Movie-API/app.js
+++ Movie-API/app.js
@@ -1,6 +1,6 @@
-var express = require("express");
-var app = express();
-var request = require("request");
+const express = require("express");
+const app = express();
+const request = require("request");
 app.set("view engine", "ejs");
 
 app.get("/", function (req,res) {
@@ -9,12 +9,12 @@ app.get("/", function (req,res) {
 })
 
 app.get("/results", function (req,res) {
-    var query = req.query.search;
-    var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb"
+    const query = req.query.search;
+    const url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb"
     request(url, function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  var data = JSON.parse(body);
+  const data = JSON.parse(body);
   res.render("results", {data: data})
         
     });
