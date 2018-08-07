e7d5257da621040e782e9c3993f2b07b0936e7d4
Firat Tale <talefirat@gmail.com>
2018-08-07 21:55:32 +0300

    adding ES6 syntax

--- Movie-API/app.js
+++ Movie-API/app.js
@@ -1,28 +1,24 @@
 const express = require("express");
-const app = express();
+const app     = express();
 const request = require("request");
 app.set("view engine", "ejs");
 
-app.get("/", function (req,res) {
+app.get("/",  (req,res) => {
     res.render("search")
     
 })
 
-app.get("/results", function (req,res) {
+app.get("/results",  (req,res) => {
     const query = req.query.search;
     const url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb"
-    request(url, function (error, response, body) {
+    request(url,  (error, response, body) => {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   const data = JSON.parse(body);
   res.render("results", {data: data})
-        
     });
+});
 
-    });
-
-
-app.listen(process.env.PORT, process.env.IP, function () {
-console.log("Movie App has started!!!"
-)}
+app.listen(3000,'localhost',  () =>
+console.log("Movie App has started!!!")
 );
